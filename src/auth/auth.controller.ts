52d0d5fe7ecc1b1f8963402bc53bfef5abbbfa3a
amine yousfi <amineyoussfi242@gmail.com>
2023-08-02 14:17:17 +0100

    refresh method added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -34,13 +34,14 @@ export class AuthController {
   @HttpCode(HttpStatus.OK) // return 200
   logout(@Req() req: Request) {
     const user = req.user;
-    return this.authService.logout(user['id']);
+    return this.authService.logout(user['sub']);
   }
 
   @UseGuards(AuthGuard('jwt-refresh'))
   @Post('refresh')
   @HttpCode(HttpStatus.OK) // return 200
-  refreshTokens() {
-    return this.authService.refreshTokens();
+  refreshTokens(@Req() req: Request) {
+    const user = req.user;
+    return this.authService.refreshTokens(user['sub'], user['refreshToken']);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -41,17 +41,31 @@ export class AuthService {
   async logout(userId: number) {
     await this.prisma.user.updateMany({
       where: {
-        id: userId, //get user by user id 
-        hashedRt: { // only if hashedRt not null
+        id: userId, //get user by user id
+        hashedRt: {
+          // only if hashedRt not null
           not: null,
         },
       },
       data: {
-        hashedRt: null, //and set it to null 
+        hashedRt: null, //and set it to null
       },
     });
   }
-  refreshTokens() {}
+  async refreshTokens(userId: number, rt: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,  
+      },
+    });
+    if (!user) throw new ForbiddenException('Access denied');
+    const rtMatches = await bcrypt.compare(rt, user.hashedRt);
+    if (!rtMatches) throw new ForbiddenException('Access denied');
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
+  }
   hashData(data: string) {
     return bcrypt.hash(data, 10);
   }
